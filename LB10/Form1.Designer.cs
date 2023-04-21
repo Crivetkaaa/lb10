1a3db9302320ed7ea01adc5d2b353b8496e3dacf
Crivetkaaa <crivetkaa@vk.com>
2023-04-21 23:06:39 +0300

    first commit

---
+++ LB10/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace LB10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сформировать отчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(609, 302);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Дата:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Отчёт";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LB10/Form1.cs
@@ -0,0 +1,153 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LB10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string date = textBox1.Text;
+            if (date == "")
+            {
+                DB db = new DB();
+                DataTable table1 = new DataTable();
+                DataTable table2 = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+                MySqlCommand command1 = new MySqlCommand("SELECT * FROM `back1`", db.getConnection());
+                adapter.SelectCommand = command1;
+                adapter.Fill(table1);
+
+                MySqlCommand command2 = new MySqlCommand("SELECT * FROM `back2`", db.getConnection());
+                adapter.SelectCommand = command2;
+                adapter.Fill(table2);
+
+                List<string> key = new List<string>();
+                for (int i = 0; i < 4; i++)
+                {
+                    key.Add(table1.Rows[i][1].ToString());
+                }
+
+                foreach (DataRow row in table2.Rows)
+                {
+                    object v = row[1];
+                    object c = row[2];
+                    object p = row[3];
+                    object d = row[4];
+                    if (key.Contains(v.ToString()))
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (table1.Rows[i][1].ToString() == v.ToString())
+                            {
+                                table1.Rows[i][2] = Int32.Parse(table1.Rows[i][2].ToString()) + Int32.Parse(c.ToString());
+                                table1.Rows[i][3] = Int32.Parse(table1.Rows[i][3].ToString()) + Int32.Parse(p.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int t = table1.Rows.Count;
+                        DataRow n = table1.NewRow();
+                        n[0] = t + 1;
+                        n[1] = v.ToString();
+                        n[2] = c.ToString();
+                        n[3] = p.ToString();
+                        n[4] = d.ToString();
+                        table1.Rows.Add(n);
+                    }
+                }
+
+                table1.Columns.Add("Общая стоимость", typeof(Int32));
+                foreach (DataRow row in table1.Rows)
+                {
+                    object c = row[2];
+                    object p = row[3];
+                    row[5] = (Int32.Parse(c.ToString()) * Int32.Parse(p.ToString()));
+                }
+
+                dataGridView1.DataSource = table1;
+            }
+            else
+            {
+                DB db = new DB();
+                DataTable table1 = new DataTable();
+                DataTable table2 = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+                MySqlCommand command1 = new MySqlCommand("SELECT * FROM `back1` WHERE `Дата` = @1", db.getConnection());
+                command1.Parameters.Add("@1", MySqlDbType.VarChar).Value = date;
+                adapter.SelectCommand = command1;
+                adapter.Fill(table1);
+
+                MySqlCommand command2 = new MySqlCommand("SELECT * FROM `back2` WHERE `Дата` = @2", db.getConnection());
+                command2.Parameters.Add("@2", MySqlDbType.VarChar).Value = date;
+                adapter.SelectCommand = command2;
+                adapter.Fill(table2);
+
+                List<string> key = new List<string>();
+                for (int i = 0; i < table1.Rows.Count; i++)
+                {
+                    key.Add(table1.Rows[i][1].ToString());
+                }
+
+                foreach (DataRow row in table2.Rows)
+                {
+                    object v = row[1];
+                    object c = row[2];
+                    object p = row[3];
+                    object d = row[4];
+                    if (key.Contains(v.ToString()))
+                    {
+                        for (int i = 0; i < table1.Rows.Count; i++)
+                        {
+                            if (table1.Rows[i][1].ToString() == v.ToString())
+                            {
+                                table1.Rows[i][2] = Int32.Parse(table1.Rows[i][2].ToString()) + Int32.Parse(c.ToString());
+                                table1.Rows[i][3] = Int32.Parse(table1.Rows[i][3].ToString()) + Int32.Parse(p.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int t = table1.Rows.Count;
+                        DataRow n = table1.NewRow();
+                        n[0] = t + 1;
+                        n[1] = v.ToString();
+                        n[2] = c.ToString();
+                        n[3] = p.ToString();
+                        n[4] = d.ToString();
+                        table1.Rows.Add(n);
+                    }
+                }
+
+                table1.Columns.Add("Общая стоимость", typeof(Int32));
+                foreach (DataRow row in table1.Rows)
+                {
+                    object c = row[2];
+                    object p = row[3];
+                    row[5] = (Int32.Parse(c.ToString()) * Int32.Parse(p.ToString()));
+                }
+
+                dataGridView1.DataSource = table1;
+            }
+        }
+    }
+}
